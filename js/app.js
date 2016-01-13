9bbcceacb07e6d3df9fa802fc535c5c81762af48
Nenad <Ahhhhmed@gmail.com>
2016-01-13 20:47:04 +0100

    add model

--- js/app.js
+++ js/app.js
@@ -1,19 +1,91 @@
+var model = {
+    init: function(){
+        this.nodesArray = [
+            {id: 1, label: '1'},
+            {id: 2, label: '2'},
+            {id: 3, label: '3'},
+            {id: 4, label: '4'},
+            {id: 5, label: '5'},
+            {id: 6, label: '6'},
+            {id: 7, label: '7'}
+        ];
+        this.edgesArray = [
+            {from: 1, to: 3, value: 4},
+            {from: 1, to: 2, value: 3},
+            {from: 2, to: 4, value: 3},
+            {from: 2, to: 5, value: 5},
+            {from: 1, to: 5, value: 1},
+            {from: 4, to: 6, value: 4},
+            {from: 5, to: 7, value: 3},
+            {from: 7, to: 6, value: 6},
+            {from: 6, to: 5, value: 4},
+        ];
+        this.colorMinimalSpaningTree();
+    },
+    getEdges: function(){
+        return this.edgesArray;
+    },
+    getNodes: function(){
+        return this.nodesArray;
+    },
+    addEdge: function(edge){
+        this.edgesArray.push(edge);
+        this.colorMinimalSpaningTree();
+    },
+    addNode: function(node){
+        nodesArray.push(node)
+    },
+    removeEdge: function(id){
+        for (i in this.edgesArray) {
+            if (this.edgesArray[i].id === id){
+                this.edgesArray.slice(i, 1);
+                break;
+            }
+        }
+    },
+    removeEdge: function(from, to){
+        for (i in this.nodesArray) {
+            if (this.nodesArray[i].from === from && this.nodesArray[i].to === to){
+                this.nodesArray.slice(i, 1);
+                break;
+            }
+        }
+    },
+    colorMinimalSpaningTree: function(){
+        this.edgesArray.sort(function (a, b){
+            return a.value - b.value;
+        });
+        var visitedEdges = {};
+        for (i in this.edgesArray) {
+            if(!(this.edgesArray[i].from in visitedEdges && this.edgesArray[i].to in visitedEdges)){
+                this.edgesArray[i].color = 'green';
+                visitedEdges[this.edgesArray[i].from] = true;
+                visitedEdges[this.edgesArray[i].to] = true;
+            }
+            else{
+                this.edgesArray[i].color = 'blue';
+            }
+        }
+    }
+};
+
+
 function startNetwork() {
     // create an array with nodes
-    nodesArray = [
-        {id: 1, label: '1', color:{background:'#FF0000'}},
+    /*nodesArray = [
+        {id: 1, label: '1'},
         {id: 2, label: '2'},
         {id: 3, label: '3'},
         {id: 4, label: '4'},
         {id: 5, label: '5'},
         {id: 6, label: '6'},
         {id: 7, label: '7'}
-    ];
-    nodes = new vis.DataSet(nodesArray);
+    ];*/
+    
 
     // create an array with edges
-    edgesArray = [
-        {from: 1, to: 3, value: 4, color:'red'},
+    /*edgesArray = [
+        {from: 1, to: 3, value: 4},
         {from: 1, to: 2, value: 3},
         {from: 2, to: 4, value: 3},
         {from: 2, to: 5, value: 5},
@@ -23,10 +95,13 @@ function startNetwork() {
         {from: 7, to: 6, value: 6},
         {from: 6, to: 5, value: 4},
 
-    ];
-    markTree(edgesArray);
-    edges = new vis.DataSet(edgesArray);
-    //markTree(edges._data);
+    ];*/
+    //markTree(edgesArray);
+
+    model.init();
+    nodes = new vis.DataSet(model.getNodes());
+    edges = new vis.DataSet(model.getEdges());
+    //markTree(edges.getDataSet());
 
     // create a network
     var container = document.getElementById('mynetwork');
@@ -46,7 +121,7 @@ function markTree(g){
     var visitedEdges = {};
     for (i in g) {
         if(!(g[i].from in visitedEdges && g[i].to in visitedEdges)){
-            g[i].color = 'red';
+            g[i].color = 'green';
             visitedEdges[g[i].from] = true;
             visitedEdges[g[i].to] = true;
         }
